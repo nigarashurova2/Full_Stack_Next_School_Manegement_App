9c202b93ba846575de835bb689e2ae246554529c
nigarashurova2 <nigarashurova4@gmail.com>
2025-06-08 00:14:28 +0400

    loading component design

--- src/app/(dashboard)/loading.tsx
+++ src/app/(dashboard)/loading.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const loading = () => {
   return (
-    <div className='w-full h-full flex justify-center items-center'>Loading...</div>
-  )
-}
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-solid"></div>
+    </div>
+  );
+};
 
-export default loading
+export default loading;
